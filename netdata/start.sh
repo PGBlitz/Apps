415e9a797e5c98af2ad43c682c09d3c90f45334f
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-05-27 09:01:57 -0400

    update

---
+++ netdata/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+# DO NOT CHANGE OR DELETE!
+app="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+app="$( echo "$app" | cut -d'/' -f4- )"
+echo $app > /pg/tmp/role.name
+################################################################################
+# STARTER CODE! If there is any code to execute before role deployment!
+
+################################################################################
+ansible-playbook /pg/apps/${app}/app.yml
+################################################################################
+# STARTER CODE! If there is any code to execute before after deployment!
+
+################################################################################
