dcb87f7e20eb91ba90c45d2a6dbd9838193a63c0
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-05-27 09:44:00 -0400

    update

--- functions.sh
+++ functions.sh
@@ -5,3 +5,17 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
+common () {
+  mkdir -p "/pg/data/{$apps}"
+
+  if [[ "apps" != "plex" ]]; then
+    chown -R 1000:1000 "/pg/data/{$apps}"
+    chmod -R 775 "/pg/data/{$apps}"
+  else
+    chown 1000:1000 "/pg/data/{$apps}"
+    chmod 775 "/pg/data/{$apps}"
+  fi 
+
+  docker stop ${apps}
+  docker rm ${apps}
+}
