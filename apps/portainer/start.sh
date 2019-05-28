f805d03d46e1aea2d1873a9becde4da4559814d5
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-05-27 21:27:17 -0400

    update

---
+++ apps/portainer/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+# DO NOT CHANGE OR DELETE!
+source /pg/apps/functions.sh
+common
+################################################################################
+# STARTER CODE! If there is any code to execute before role deployment!
+
+################################################################################
+ansible-playbook "/pg/apps/apps/${apps}/app.yml"
+################################################################################
+# ENDING CODE! If there is any code to execute before after deployment!
+
+################################################################################
