2c288a4e2fb4b4e897844eaf79833e8b6fda25a5
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-06-05 13:52:24 -0400

    update

--- apps/pgui/mountcheck.sh
+++ apps/pgui/mountcheck.sh
@@ -5,9 +5,9 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-mkdir -p /pg/data/blitz/emergency
+mkdir -p /pg/var/emergency
 mkdir -p /pg/data/blitz
-rm -rf /pg/data/blitz/emergency/*
+rm -rf /pg/var/emergency/*
 sleep 15
 diskspace27=0
 
@@ -15,21 +15,21 @@ while true
 do
 
 # GDrive
-if [[ $(rclone lsd --config /pg/data/blitz/rclone.conf gdrive: | grep "\<plexguide\>") == "" ]]; then
+if [[ $(rclone lsd --config /pg/var/rclone/blitz.conf gdrive: | grep "\<plexguide\>") == "" ]]; then
   echo "🔴 Not Operational "> /pg/var/pg.gdrive; else echo "✅ Operational " > /pg/var/pg.gdrive; fi
 
 if [[ $(ls -la /mnt/gdrive | grep "plexguide") == "" ]]; then
   echo "🔴 Not Operational"> /pg/var/pg.gmount; else echo "✅ Operational" > /pg/var/pg.gmount; fi
 
 # TDrive
-if [[ $(rclone lsd --config /pg/data/blitz/rclone.conf tdrive: | grep "\<plexguide\>") == "" ]]; then
+if [[ $(rclone lsd --config /pg/var/rclone/blitz.conf tdrive: | grep "\<plexguide\>") == "" ]]; then
   echo "🔴 Not Operational"> /pg/var/pg.tdrive; else echo "✅ Operational" > /pg/var/pg.tdrive; fi
 
 if [[ $(ls -la /mnt/tdrive | grep "plexguide") == "" ]]; then
   echo "🔴 Not Operational "> /pg/var/pg.tmount; else echo "✅ Operational" > /pg/var/pg.tmount; fi
 
 # Union
-if [[ $(rclone lsd --config /pg/data/blitz/rclone.conf pgunion: | grep "\<plexguide\>") == "" ]]; then
+if [[ $(rclone lsd --config /pg/var/rclone/blitz.conf pgunion: | grep "\<plexguide\>") == "" ]]; then
   echo "🔴 Not Operational "> /pg/var/pg.union; else echo "✅ Operational" > /pg/var/pg.union; fi
 
 if [[ $(ls -la /mnt/unionfs | grep "plexguide") == "" ]]; then
@@ -42,7 +42,7 @@ leftover=$(df /pg/data/blitz | tail -n +2 | awk '{print $4}')
 
 if [[ "$leftover" -lt "3000000" ]]; then
   diskspace27=1
-  echo "Emergency: Primary DiskSpace Under 3GB - Stopped Media Programs & Downloading Programs (i.e. Plex, NZBGET, RuTorrent)" > /pg/data/blitz/emergency/message.1
+  echo "Emergency: Primary DiskSpace Under 3GB - Stopped Media Programs & Downloading Programs (i.e. Plex, NZBGET, RuTorrent)" > /pg/var/emergency/message.1
   docker stop plex 1>/dev/null 2>&1
   docker stop emby 1>/dev/null 2>&1
   docker stop jellyfin 1>/dev/null 2>&1
@@ -60,48 +60,48 @@ elif [[ "$leftover" -gt "3000000" && "$diskspace27" == "1" ]]; then
   docker start rutorrent 1>/dev/null 2>&1
   docker start deluge 1>/dev/null 2>&1
   docker start qbitorrent 1>/dev/null 2>&1
-  rm -rf /pg/data/blitz/emergency/message.1
+  rm -rf /pg/var/emergency/message.1
   diskspace27=0
 fi
 
 ##### Warning for Ports Open with Traefik Deployed
 if [[ $(cat /pg/var/pg.ports) != "Closed" && $(docker ps --format '{{.Names}}' | grep "traefik") == "traefik" ]]; then
-  echo "Warning: Traefik deployed with ports open! Server at risk for explotation!" > /pg/data/blitz/emergency/message.a
-elif [ -e "/pg/data/blitz/emergency/message.a" ]; then rm -rf /pg/data/blitz/emergency/message.a; fi
+  echo "Warning: Traefik deployed with ports open! Server at risk for explotation!" > /pg/var/emergency/message.a
+elif [ -e "/pg/var/emergency/message.a" ]; then rm -rf /pg/var/emergency/message.a; fi
 
 if [[ $(cat /pg/var/pg.ports) == "Closed" && $(docker ps --format '{{.Names}}' | grep "traefik") == "" ]]; then
   echo "Warning: Apps Cannot Be Accessed! Ports are Closed & Traefik is not enabled!"
-  echo "Either deploy traefik or open your ports (which is worst for security)" > /pg/data/blitz/emergency/message.b
-elif [ -e "/pg/data/blitz/emergency/message.b" ]; then rm -rf /pg/data/blitz/emergency/message.b; fi
+  echo "Either deploy traefik or open your ports (which is worst for security)" > /pg/var/emergency/message.b
+elif [ -e "/pg/var/emergency/message.b" ]; then rm -rf /pg/var/emergency/message.b; fi
 ##### Warning for Bad Traefik Deployment - message.c is tied to traefik showing a status! Do not change unless you know what your doing
-touch /pg/data/blitz/traefik.check
+touch /pg/var/traefik.check
 domain=$(cat /pg/var/server.domain)
-wget -q "https://portainer.${domain}" -O "/pg/data/blitz/traefik.check"
-if [[ $(cat /pg/data/blitz/traefik.check) == "" && $(docker ps --format '{{.Names}}' | grep traefik) == "traefik" ]]; then
-  echo "Traefik is Not Deployed Properly! Cannot Reach the Portainer SubDomain!" > /pg/data/blitz/emergency/message.c
+wget -q "https://portainer.${domain}" -O "/pg/var/traefik.check"
+if [[ $(cat /pg/var/traefik.check) == "" && $(docker ps --format '{{.Names}}' | grep traefik) == "traefik" ]]; then
+  echo "Traefik is Not Deployed Properly! Cannot Reach the Portainer SubDomain!" > /pg/var/emergency/message.c
 else
-  if [ -e "/pg/data/blitz/emergency/message.c" ]; then
-  rm -rf /pg/data/blitz/emergency/message.c; fi
+  if [ -e "/pg/var/emergency/message.c" ]; then
+  rm -rf /pg/var/emergency/message.c; fi
 fi
 ##### Warning for Traefik Rate Limit Exceeded
-if [[ $(cat /pg/data/blitz/traefik.check) == "" && $(docker logs traefik | grep "rateLimited") != "" ]]; then
+if [[ $(cat /pg/var/traefik.check) == "" && $(docker logs traefik | grep "rateLimited") != "" ]]; then
   echo "$domain's rated limited exceed | Traefik (LetsEncrypt)! Takes upto one week to"
-  echo "clear up (or use a new domain)" > /pg/data/blitz/emergency/message.d
+  echo "clear up (or use a new domain)" > /pg/var/emergency/message.d
 else
-  if [ -e "/pg/data/blitz/emergency/message.d" ]; then
-  rm -rf /pg/data/blitz/emergency/message.d; fi
+  if [ -e "/pg/var/emergency/message.d" ]; then
+  rm -rf /pg/var/emergency/message.d; fi
 fi
 
 ################# Generate Output
 echo "" > /pg/var/emergency.log
 
-if [[ $(ls /pg/data/blitz/emergency) != "" ]]; then
+if [[ $(ls /pg/var/emergency) != "" ]]; then
 countmessage=0
 while read p; do
   let countmessage++
   echo -n "${countmessage}. " >> /pg/var/emergency.log
-  echo "$(cat /pg/data/blitz/emergency/$p)" >> /pg/var/emergency.log
-done <<< "$(ls /pg/data/blitz/emergency)"
+  echo "$(cat /pg/var/emergency/$p)" >> /pg/var/emergency.log
+done <<< "$(ls /pg/var/emergency)"
 else
   echo "NONE" > /pg/var/emergency.log
 fi
