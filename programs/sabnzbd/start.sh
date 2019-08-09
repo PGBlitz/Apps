878c1e9ce57b713b84bf220febade23ce30c18ff
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-08 20:46:29 -0400

    adding sabnzbd

---
+++ programs/sabnzbd/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+# DO NOT CHANGE OR DELETE!
+source /pg/apps/functions.sh
+apps="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+apps="$( echo "$apps" | cut -d'/' -f5- )"
+common
+################################################################################
+# STARTER CODE! If there is any code to execute before role deployment!
+
+################################################################################
+ansible-playbook "/pg/apps/programs/${apps}/app.yml"
+################################################################################
+# ENDING CODE! If there is any code to execute before after deployment!
+
+################################################################################
